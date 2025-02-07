6ff1d2c0a3c256bfbd7cd74e1b1974c53a2de385
Daniyal <daniyalmahmood123@gmail.com>
2025-02-08 04:39:48 +0530

    fix(api): Revise error handling

--- src/main/java/com/dm/ecommerce/config/OpenApiConfig.java
+++ src/main/java/com/dm/ecommerce/config/OpenApiConfig.java
@@ -1,5 +1,5 @@
 package com.dm.ecommerce.config;
-
+      // Updated on 2025-02-08 - Commit activity
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
