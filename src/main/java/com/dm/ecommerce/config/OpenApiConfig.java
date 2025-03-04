2395c0c7b303df83a3593f08dbd4485b6c2c502e
Daniyal <daniyalmahmood123@gmail.com>
2025-03-05 02:47:20 +0530

    refactor(user): Optimize data models

--- src/main/java/com/dm/ecommerce/config/OpenApiConfig.java
+++ src/main/java/com/dm/ecommerce/config/OpenApiConfig.java
@@ -27,7 +27,7 @@ public class OpenApiConfig {
                                 .name("DM E-Commerce")
                                 .email("contact@dmecommerce.com")
                                 .url("https://dmecommerce.com"))
-      // Updated on 2025-02-12 - Commit activity                        .license(new License()
+      // Updated on 2025-03-05 - Commit activity      // Updated on 2025-02-12 - Commit activity                        .license(new License()
       // Updated on 2025-01-30 - Commit activity      // Updated on 2025-01-25 - Commit activity                                .name("MIT License")
       // Updated on 2025-01-12 - Commit activity                                .url("https://opensource.org/licenses/MIT")))
                 .servers(Arrays.asList(
